fea8b663f658269d06493bde1a9ca6c3e1cfd306
Jacke20 <jackeaik@hotmail.com>
2015-03-02 14:33:02 +0100

    added first version of header and footer

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,9 +1,41 @@
 @extends('master')
 
-@section('head-title')
-    
+@section('head-extra')
+    <link href="styles/style.css" rel="stylesheet">
+	<meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 @stop
 
 @section('body')
-    <h1>Välkommen till Jacobis lilla värld (jag är arg)</h1>
+    <nav class="navbar clearfix">
+			<div class="logo">Logga</div>
+			<ul class="searchbar clearfix">
+				<li><form class="search-form" role="search">
+            		   <input type="text" class="text-area" placeholder="Söktext">
+                   	   <button type="submit" class="btn grow">Sök</button>
+         		</form></li>
+			</ul>
+			<ul class="menu clearfix">
+				<li><a href="#">Hem</a></li>
+				<li>
+					<a href="#">Användare</a>
+					<!--
+					<ul class="menu hidden">
+						<li><a href="#">Nurse</a></li>
+						<li><a href="#">Doctor</a></li>
+					</ul>-->
+				</li>
+				<li><a href="#">Logga in</a></li>
+			</ul>
+	</nav>
+	<div id="container">
+		<div id="content">
+		<!-- Content goes here-->
+		</div>
+	</div>
+	<div class="push"></div>
+	<div id="footer" class="clearfix">
+			<a href="#" class="pull-left" id="about">Om oss</a>
+			<a href="#" class="pull-right" id="contact">Kontakta oss</a>
+	</div>
 @stop
