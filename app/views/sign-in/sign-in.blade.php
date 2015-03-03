f0837263732ca8c3435f9151a76c479f54545c49
Jonas Dahl <jonas@jdahl.se>
2015-03-03 09:45:09 +0100

    updated login page - no functionality

--- app/views/sign-in/sign-in.blade.php
+++ app/views/sign-in/sign-in.blade.php
@@ -10,23 +10,4 @@
 
 @section('body')
 	<h1>Logga in</h1>
-	{{ Form::model('pm', 567) }}
-		<table>
-			<tr>
-				<td>Användarnamn</td>
-				<td>{{ Form::text('title') }}</td>
-			</tr>
-			<tr>
-				<td>Innehåll</td>
-				<td>{{ Form::text('content') }}</td>
-			</tr>
-			<tr>
-				<td>Lösenord</td>
-				<td><input type="password" /></td>
-			</tr>
-			<tr>
-				<td colspan="2"><input type="submit" value="Logga in" /></td>
-			</tr>
-		</table>
-	{{ Form::close() }}
 @stop
