8be0142d6e8605508219d3fa1cfd87e704803a27
Jacke20 <jackeaik@hotmail.com>
2015-03-04 21:48:18 +0100

    "Meny"-button should now work properly (no slide animation)
    Also content shouldn't be placed behind header/footer by default now

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -5,6 +5,14 @@
 	<title>@yield('head-title')</title>
     <link href="/styles/style.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="/js/jquery-1.11.2.js"></script>
+	<script type="text/javascript">
+  		$(function() {
+      			$('.menu-btn').on('click', function(){
+              	$('.responsive-menu').toggleClass('expand');
+      		});
+    	});
+  </script>
 
 	@yield('head-extra')
 </head>
@@ -19,16 +27,20 @@
                    	   <button type="submit" class="btn grow">Sök</button>
          		</form></li>
 			</ul>
-			<button class="visible-xs" id="burger">Meny</button>
-			<ul class="menu clearfix">
-				<li><a href="{{ URL::route('index') }}">Hem</a></li>
-				@if(Auth::guest())
-					<li><a href="{{ URL::route('sign-in') }}">Logga in</a></li>
-				@else
-					<li><a href="{{ URL::route('user', array(Auth::user())) }}">Min sida</a></li>
-					<li><a href="{{ URL::route('sign-out') }}">Logga ut</a></li>
-				@endif
-			</ul>
+			<div class="menu-btn">
+				<button class="visible-xs" id="burger">Meny</button>
+			</div>
+			<div class="responsive-menu">
+				<ul class="menu clearfix">
+					<li><a href="{{ URL::route('index') }}">Hem</a></li>
+					@if(Auth::guest())
+						<li><a href="{{ URL::route('sign-in') }}">Logga in</a></li>
+					@else
+						<li><a href="{{ URL::route('user', array(Auth::user())) }}">Min sida</a></li>
+						<li><a href="{{ URL::route('sign-out') }}">Logga ut</a></li>
+					@endif
+				</ul>
+			</div>
 	</nav>
 	<div id="container">
 		<div id="content">
