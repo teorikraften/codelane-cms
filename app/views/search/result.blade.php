dc4ca656639a65cb8007d44b3bdbb2a55191d809
Jonas Dahl <jonas@jdahl.se>
2015-03-05 17:35:06 +0100

    added example pm page

--- app/views/search/result.blade.php
+++ app/views/search/result.blade.php
@@ -9,12 +9,12 @@
     <h2 class="search">Sökning: {{ $searchQuery }}</h2>
     <ul class="result">
     	<li>
-    		<h3><a href="{{ URL::route('pm-show', 'hjartklinikens-papperskorgar') }}">Hjärtklinikens papperskorgar</a></h3>
+    		<h3><a href="{{ URL::route('pm-show', 'hjärtklinikens-papperskorgar') }}">Hjärtklinikens papperskorgar</a></h3>
     		<p class="description">Alla Hjärtklinikens papperskorgar ska vara röda och tillverkade av 
     			Johan Lantz ... om Jacobi har gjort papperskorgen ska detta utmärkas tydligt.</p>
     	</li>
     	<li>
-    		<h3><a href="{{ URL::route('pm-show', 'bokning-av-motesrum') }}">Bokning av mötesrum</a></h3>
+    		<h3><a href="{{ URL::route('pm-show', 'bokning-av-mötesrum') }}">Bokning av mötesrum</a></h3>
     		<p class="description">... om ett mötesrum bokas av Andy kommer det förmodligen inte vara bokat länge till.
     			Detta eftersom Andy ibland glömmer av boka. Eric brukar glömma att kvittera.</p>
     	</li>
