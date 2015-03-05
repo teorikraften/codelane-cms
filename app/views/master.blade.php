094e8da4bda68f9fa957f012797990992874abd5
Jonas Dahl <jonas@jdahl.se>
2015-03-05 12:20:21 +0100

    fixed "min sida" bug

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -37,7 +37,7 @@
 						<li><a href="{{ URL::route('sign-in') }}">Logga in</a></li>
 						<li><a href="{{ URL::route('sign-up') }}">Registrera dig</a></li>
 					@else
-						<li><a href="{{ URL::route('user', array(Auth::user())) }}">Min sida</a></li>
+						<li><a href="{{ URL::route('user', array(Auth::user()->id)) }}">Min sida</a></li>
 						<li><a href="{{ URL::route('sign-out') }}">Logga ut</a></li>
 					@endif
 				</ul>
