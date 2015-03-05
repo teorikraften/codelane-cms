c95c3214785c2a94190a50f53da04f1405a4e381
Jonas Dahl <jonas@jdahl.se>
2015-03-05 12:19:21 +0100

    gave sign in page a new design

--- app/views/sign-in/sign-in.blade.php
+++ app/views/sign-in/sign-in.blade.php
@@ -20,15 +20,20 @@
 	@endif
 	<div class="form">
 		<div class="row">
-			<div class="description">E-postadress:</div>
-			<div class="input">{{ Form::text('email') }}</div>
+			<div class="description">{{ Form::label('email', 'E-postadress') }}</div>
+			<div class="input">{{ Form::text('email', NULL, array('class' => 'text')) }}</div>
 		</div>
 		<div class="row">
-			<div class="description">Lösenord:</div>
-			<div class="input">{{ Form::text('password') }}</div>
+			<div class="description">{{ Form::label('password', 'Lösenord') }}</div>
+			<div class="input">{{ Form::password('password', array('class' => 'text')) }}</div>
+		</div>
+		<div class="row same">
+			<div class="input">{{ Form::checkbox('remember', 'yes', false, array('class' => 'checkbox', 'id' => 'remember')) }}</div>
+			<div class="description">{{ Form::label('remember', 'Kom ihåg mig') }}</div>
+			<div class="clear"></div>
 		</div>
 		<div class="submit">
-			{{ Form::submit('Logga in') }}
+			{{ Form::submit('Logga in', array('class' => 'submit')) }}
 		</div>
 	</div>
 	{{ Form::close() }}
