cac147f783535e6c452785fae733906b77861456
Jonas Dahl <jonas@jdahl.se>
2015-03-19 18:59:01 +0100

    created submenu for admin

--- app/views/user/admin/tags.blade.php
+++ app/views/user/admin/tags.blade.php
@@ -4,6 +4,24 @@
     Alla taggar
 @stop
 
+@section('submenu')
+	@include('includes.admin-menu')
+@stop
+
 @section('body')
-    
+    <h1>Taggar</h1>
+    <table>
+		<tr>
+			<th></th>
+			<th></th>
+			<th>Tagg</th>
+		</tr>
+    	@foreach($tags as $tag)
+    		<tr>
+    			<td><a href="">Ändra</a></td>
+    			<td><a href="">Ta bort</a></td>
+    			<td>{{ $tag->name }}</td>
+    		</tr>
+    	@endforeach
+    </table>
 @stop
