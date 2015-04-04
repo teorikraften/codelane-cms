7a637aabf62030ed20ac8b42fd3d7eabfbc67eb1
Andy92 <andyy@kth.se>
2015-04-04 12:58:26 +0200

    first version of information window
    needs more style, and lots of improvement

---
+++ public/js/infoWindow.js
@@ -0,0 +1,28 @@
+function toggle(div_id) {
+	var el = document.getElementById(div_id);
+	if ( el.style.display == 'none' ) {	el.style.display = 'block';}
+	else {el.style.display = 'none';}
+}
+function window_pos(infoWindow) {
+	if (typeof window.innerWidth != 'undefined') {
+		viewportwidth = window.innerHeight;
+	} else {
+		viewportwidth = document.documentElement.clientHeight;
+	}
+	if ((viewportwidth > document.body.parentNode.scrollWidth) && (viewportwidth > document.body.parentNode.clientWidth)) {
+		window_width = viewportwidth;
+	} else {
+		if (document.body.parentNode.clientWidth > document.body.parentNode.scrollWidth) {
+			window_width = document.body.parentNode.clientWidth;
+		} else {
+			window_width = document.body.parentNode.scrollWidth;
+		}
+	}
+	var infoWindow = document.getElementById(infoWindow);
+	window_width=window_width/2-150;// center the window.
+	infoWindow.style.left = window_width + 'px';
+}
+
+	window_pos(windowname);
+	toggle(windowname);		
+}
