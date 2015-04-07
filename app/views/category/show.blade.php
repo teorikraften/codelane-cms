be0e0303dd3c4b28f331a53806b6e0b0f0580963
Adrian Lökk <adrian.lokk94@gmail.com>
2015-04-07 21:18:25 +0200

    role notifications - multiple roles

--- app/views/category/show.blade.php
+++ app/views/category/show.blade.php
@@ -61,7 +61,12 @@ Kategori {{ $category or '' }}
 				@if (isset($pm['roles']))
 					<?php $roles = 'Detta PM är relevant till en eller flera av dina roller: '; ?>
 					@foreach ($pm['roles'] as $key => $role)
-						<?php $roles = $roles . $role->name ?>
+						{{ $key }}
+						@if($key == 0)
+							<?php $roles = $roles . $role->name; ?>
+						@else
+							<?php $roles = $roles . $role->name . ', '; ?>
+						@endif
 					@endforeach
 					{{ HTML::image('images/persons.png', $roles, array('title' => $roles)) }} 
 				@endif
